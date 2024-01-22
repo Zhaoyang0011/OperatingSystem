c5eb47b6816b7be99f9b04248c90fddc4d9d17b8
szy <szy@source-learn.myguest.virtualbox.org>
2024-01-22 15:25:02 -0500

    set up x86_64

---
+++ include/type.h
@@ -0,0 +1,28 @@
+#ifndef _TYPE_H
+#define _TYPE_H
+
+#define KLINE static inline
+
+#define NULL ((void *)0) // null pointer
+
+typedef unsigned char uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned int uint32_t;
+typedef unsigned long long uint64_t;
+typedef unsigned int size_t;
+typedef unsigned long uint_t;
+
+typedef char sint8_t;
+typedef short sint16_t;
+typedef int sint32_t;
+typedef long long sint64_t;
+typedef long sint_t;
+typedef unsigned long addr_t;
+
+#define ALIGN(x, a) (((x) + (a) - 1) & ~((a) - 1))
+#define P4K_ALIGN(x) ALIGN(x,0x1000)
+
+#define TRUE 1
+#define FALSE 0
+
+#endif
---
+++ src/boot/x86_64/boot.asm
@@ -0,0 +1,76 @@
+ORG 0x7c00
+
+SETUP_START equ 0x500
+
+[section .text]
+[BITS 16]
+_start:
+    jmp short start
+    nop
+    times 0x21 db 0
+
+start:
+    cli
+    mov ax, 0
+    mov ss, ax
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov sp, 0x7c00
+    sti
+
+    mov ebx, 1
+    mov ecx, 4
+    mov edi, SETUP_START
+    call ata_lba_read
+
+    jmp 0x0:SETUP_START
+
+ata_lba_read:
+    mov eax, ebx
+    ; LBA low
+    mov dx, 0x1f3
+    out dx, al
+
+    ; LBA mid
+    mov dx, 0x1f4
+    shr eax, 8
+    out dx, al
+
+    ; LBA high
+    mov dx, 0x1f5
+    shr eax, 16
+    out dx, al
+
+    mov dx, 0x1f6
+    shr eax, 8
+    and al, 0x0f
+    or al, 0xe0
+    out dx, al
+    
+    mov dx, 0x1f7
+    mov al, 0x20
+    out dx, al
+
+.next_sector:
+    push ecx
+
+    mov dx, 0x1f7
+.check_hd:
+    in al, dx
+    test al, 8
+    jz .check_hd
+
+.read_hd:
+    mov ecx, 256
+    mov dx, 0x1f0
+    rep insw
+
+    pop ecx
+    loop .next_sector
+
+    ret
+
+times 510 - ($ - $$) db 0
+db 0x55, 0xaa
