ec58dfa6b000b46a73543a8645102ecae162df62
szy <szy@source-learn.myguest.virtualbox.org>
2024-04-12 20:35:52 -0400

    Create implementing physical memory allocate and free file!

---
+++ include/hal/memory/memdivmer.h
@@ -0,0 +1,8 @@
+#ifndef _MEMDIVMER_H
+#define _MEMDIVMER_H
+
+#include <hal/memory/mempage.h>
+
+mpdesc_t *memory_divide();
+
+#endif
---
+++ src/hal/x86_64/memory/memdivmer.c
@@ -0,0 +1,6 @@
+#include <hal/memory/memdivmer.h>
+
+mpdesc_t *memory_divide()
+{
+    return NULL;
+}
